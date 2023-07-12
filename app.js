26e0f796d11ede19e5e7ccc2aff4402fa5e6563c
RafaelDangelo <rafaelrda@yahoo.com.br>
2023-07-12 17:07:53 -0300

    adicionando um console log

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 console.log("Rodando o sistema de gerenciamento de jogos!")
 //mais um console log de teste
 console.log("Feito o commit sem ninguém ver na aula")
+console.log("Enviando para o GitHub")
 //Fim

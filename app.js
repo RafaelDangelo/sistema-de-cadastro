4dc28626e750113c43927db33e2db8df38637f7c
RafaelDangelo <rafaelrda@yahoo.com.br>
2023-08-04 10:14:07 -0300

    Voltando ao estado de alteração sem ninguém ver

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 console.log("Rodando o sistema de gerenciamento de jogos!")
 //mais um console log de teste
 console.log("Feito o commit sem ninguém ver na aula")
-console.log("Enviando para o GitHub")
 //Fim
